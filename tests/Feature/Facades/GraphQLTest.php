0325808b4f1f2cc72774fd72ec6203e07360b8d0
cyrille <coquardcyr@gmail.com>
2025-04-23 08:24:58 +0200

    Added a new test for variables

--- tests/Feature/Facades/GraphQLTest.php
+++ tests/Feature/Facades/GraphQLTest.php
@@ -43,4 +43,67 @@ class GraphQLTest extends TestCase
 
         $this->assertArrayHasKey('entity', $response);
     }
+
+    public function testGraphQLClientWithVariables() {
+        Http::fake([
+            'localhost*' => function (Request $request) {
+                if(! $request->isJson()) {
+                    return null;
+                }
+
+                if( ! key_exists('query', $request->data())) {
+                    return null;
+                }
+
+                if($request->data()['query'] !== "query createUser (\$email: String!, \$firstname: String!, \$lastname: String!, \$password: String!) {
+		createUser(email: \$email, firstname: \$firstname, passwd: \$password, lastname: \$lastname) {
+			email
+			firstname
+			lastname
+			active
+	}
+}") {
+                    return null;
+                }
+
+                if(! key_exists('variables', $request->data())) {
+                    return null;
+                }
+
+
+                if(array_diff([
+                    'email' => 'my@email.com',
+                    'firstname' => 'John',
+                    'lastname' => 'Doe',
+                    'passwd' => 'password',
+                ], $request->data()['variables']) !== []) {
+                    return null;
+                }
+
+                return Http::response([
+                    'data' => [
+                        'entity' => [
+                            'email' => 'my@email.com',
+                            'name' => 'John Doe',
+                        ]
+                    ]
+                ]);
+            }
+        ]);
+        $response = GraphQL::endpoint("http://localhost/endpoint")->raw('query createUser ($email: String!, $firstname: String!, $lastname: String!, $password: String!) {
+		createUser(email: $email, firstname: $firstname, passwd: $password, lastname: $lastname) {
+			email
+			firstname
+			lastname
+			active
+	}
+}')->with([
+            'email' => 'my@email.com',
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'passwd' => 'password',
+        ])->get();;
+
+        $this->assertArrayHasKey('entity', $response);
+    }
 }
